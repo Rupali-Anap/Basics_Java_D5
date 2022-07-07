4db8e044ab8356f73e01260117a466175268e36c
Rupali-Anap <anaprupali333@gmail.com>
2022-07-07 19:27:23 +0530

    solution is added

---
+++ power_2.java
@@ -0,0 +1,16 @@
+package com.briedgelabz.basics;
+import java.util.Scanner;
+public class power_2 {
+
+        public static void main(String[] args) {
+
+            Scanner num = new Scanner(System.in);
+            System.out.println("Enter The number");
+            for (int a = num.nextInt(); a <= 31; ++a) {
+                for (int i = 1; i <= a; ++i) {
+                    System.out.println("Power Of 2 is " + i + "=" + 2 * i);
+                }
+            }
+
+    }
+}
